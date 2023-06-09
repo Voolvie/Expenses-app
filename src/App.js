3a5aa0c0ca47a346711df90f91eb061aa53aceb9
unknown <sklemcz@gmail.com>
2023-06-09 14:06:54 +0200

    new changes

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+import Card from "./components/Card";
+
+function App() {
+  const expenses = [
+    {
+      title: "bar1",
+      date: new Date(2021, 2, 22),
+      amount: 21.37,
+    },
+    { title: "bar2", date: new Date(2021, 4, 12), amount: 2 },
+    { title: "bar3", date: new Date(2021, 6, 18), amount: 2 },
+  ];
+  return (
+    <Card className="expenses">
+      <ExpenseItem title={expenses[0].title} date={expenses[0].date} amount={expenses[0].amount}/>
+      <ExpenseItem title={expenses[1].title} date={expenses[1].date} amount={expenses[1].amount}/>
+      <ExpenseItem title={expenses[2].title} date={expenses[2].date} amount={expenses[2].amount}/>
+    </Card>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+function ExpenseItem(props) {
+    let title = props.title
+
+    const clickHandler = () => {
+        title ="Updated"
+    }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>klick</button>
+    </Card>
+  );
+}
+export default ExpenseItem;
