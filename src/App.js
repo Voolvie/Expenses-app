a19006a9a415c71c3e70958b35ff545a77281e64
unknown <sklemcz@gmail.com>
2023-06-15 14:25:17 +0200

    dynamic expenses + filter

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import Card from "./components/Card";
 import NewExpense from "./components/NewExpense/NewExpense";
 import ExpensesFilter from "./components/ExpensesFilter";
 
+const INITIAL_EXPENSES = [
+  {
+    id: 1,
+    title: "Kitchen",
+    date: new Date(2021, 2, 22),
+    amount: 21.37,
+  },
+  { id:2, title: "Sink", date: new Date(2021, 4, 12), amount: 24 },
+  { id:3, title: "Toilet", date: new Date(2021, 6, 18), amount: 542 },
+];
+
 function App(props) {
   const [filteredYear, setFilteredYear] = useState('2020')
-  const expenses = [
-    {
-      title: "bar1",
-      date: new Date(2021, 2, 22),
-      amount: 21.37,
-    },
-    { title: "bar2", date: new Date(2021, 4, 12), amount: 2 },
-    { title: "bar3", date: new Date(2021, 6, 18), amount: 2 },
-  ];
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES)  
+
+  const filteredExpenses = expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
   }
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear)
@@ -25,11 +35,11 @@ function App(props) {
   return (
     <div>
     <Card className="expenses">
+    <NewExpense onAddExpense={addExpenseHandler} />
     <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <ExpenseItem title={expenses[0].title} date={expenses[0].date} amount={expenses[0].amount}/>
-      <ExpenseItem title={expenses[1].title} date={expenses[1].date} amount={expenses[1].amount}/>
-      <ExpenseItem title={expenses[2].title} date={expenses[2].date} amount={expenses[2].amount}/>
+    {filteredExpenses.map((expense) =>
+       <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount}/>
+       )}
     </Card>
     </div>
   );
